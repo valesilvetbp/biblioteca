50eba8981b1d29874768bf5733b62763d0e7c4b8
Ugo Palatucci <upalatucci@gmail.com>
2021-11-18 14:36:39 +0100

    Roboto

--- packages/mars-theme/src/components/index.js
+++ packages/mars-theme/src/components/index.js
@@ -23,10 +23,17 @@ const Theme = ({ state }) => {
   return (
     <>
       {/* Add some metatags to the <head> of the HTML. */}
+
       <Title />
       <Head>
         <meta name="description" content={state.frontity.description} />
         <html lang="en" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,300;0,400;0,700;1,300;1,400;1,700&display=swap"
+          rel="stylesheet"
+        ></link>
       </Head>
 
       {/* Add some global styles for the whole site, like body or a's. 
@@ -57,7 +64,7 @@ export default connect(Theme);
 const globalStyles = css`
   body {
     margin: 0;
-    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto,
+    font-family: "Roboto", -apple-system, BlinkMacSystemFont, "Segoe UI",
       "Droid Sans", "Helvetica Neue", Helvetica, Arial, sans-serif;
   }
   a,
